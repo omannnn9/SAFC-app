3ecb48e660e711e983fcdb5722a32f34f1fa23d1
omannnn9 <dilloooman26@gmail.com>
2026-06-09 20:51:03 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+importer { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
